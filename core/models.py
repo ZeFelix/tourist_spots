fbb68a39c1d94cbc900f021050a0ca40b5fc3bf1
Félix <jophev3@hotmail.com>
2019-09-07 08:23:07 -0300

    added relationship manytomany in core

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from attractions.models import Attractions
+
 
 class TouristSpots(models.Model):
 
     name = models.CharField(max_length=50)
     description = models.TextField()
     approved = models.BooleanField(default=False)
+    attractions = models.ManyToManyField(Attractions)
 
     def __str__(self):
 
